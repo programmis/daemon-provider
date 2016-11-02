550970dcca535a34b43aa5f83c284eec6aae554f
Daniil Sazhin <sazhin_daniil@mail.ru>
2016-11-02 11:08:38 +0300

    add empty daemon provider test

---
+++ tests/DaemonProviderTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daniil
+ * Date: 02.11.16
+ * Time: 11:07
+ */
+
+namespace tests;
+
+/**
+ * Class DaemonProviderTest
+ *
+ * @package tests
+ */
+class DaemonProviderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+    }
+}
